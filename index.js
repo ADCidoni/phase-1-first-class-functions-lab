151a3d6014cabc981e5ad5833cc9167a63afb4e4
April <a.d.cidoni@gmail.com>
2023-11-30 16:31:12 -0500

    phase-1-first-class-functions

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solution in this file!
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo']
+const returnFirstTwoDrivers = function() {
+    return drivers.slice(0, 2)
+}
+const returnLastTwoDrivers = function() {
+    return drivers.slice(2,4)
+}
+let selectingDrivers = [returnFirstTwoDrivers,returnLastTwoDrivers]
+
+const createFareMultiplier = (num1) => (num2) => num1 * num2;
+const fareDoubler = createFareMultiplier(2);
+const fareTripler = createFareMultiplier(3);
+function selectDifferentDrivers(drivers, callback) { 
+    if(callback === returnFirstTwoDrivers) {
+        return drivers.slice(0, 2)
+    }else if(callback === returnLastTwoDrivers) {
+        return drivers.slice(2, 4,)
+    }
+        
+    }
